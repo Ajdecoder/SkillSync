eacb27cd2b498635da671d242536ae8ecee4d669
Anuj <anujshrivastav530@gmail.com>
2024-09-04 20:56:48 +0530

    cors origin changes

--- client/src/commonClient.js
+++ client/src/commonClient.js
@@ -1,4 +1,4 @@
 const localhost = "http://localhost:9002";
-const prod_port = 'https://app-skillsync.vercel.app/';
+const prod_port = "https://skillsync-c521.onrender.com";
 
 export const PORT = prod_port ? prod_port : localhost;
