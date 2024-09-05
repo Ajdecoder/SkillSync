2df0ecdb144cab21f9f2fcbcbf2f7ca563b59d72
Anuj Kumar Shrivastav <123423315+Ajdecoder@users.noreply.github.com>
2024-09-05 22:18:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,12 +11,13 @@ const app = express();
 app.use(express.static("public"));
 const PORT = process.env.PORT || 9002;
 
-app.use(
-  cors({
-    origin: "https://app-skillsync.vercel.app",
-    credentials: true,
-  })
-);
+const corsOptions = {
+    origin: 'https://app-skillsync.vercel.app', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, 
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
