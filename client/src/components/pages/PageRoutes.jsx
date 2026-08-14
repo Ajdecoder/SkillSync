a240753359350181183a409a929cba07033cc0b3
Anuj <anujshrivastav530@gmail.com>
2026-08-14 19:11:11 +0530

    updates

--- client/src/components/pages/PageRoutes.jsx
+++ client/src/components/pages/PageRoutes.jsx
@@ -40,7 +40,7 @@ import { ChooseLoginMode } from "../Login/ChooseLoginMode.jsx";
 import { LoginCandidate } from "../Login/LoginCandidate.jsx";
 import { LoginRecruiter } from "../Login/LoginRecruiter.jsx";
 import { Register } from "../Register/ChooseRegisterMode.jsx";
-import { RegCandidate } from "../Register/Regcandidate?.jsx";
+import { RegCandidate } from "../Register/Regcandidate.jsx";
 import { RegRecruiter } from "../Register/RegRecruiter.jsx";
 
 // Candidate Pages
@@ -49,7 +49,7 @@ import { AllBookmarks } from "../Requirements/Candidates/AllBookmarks.jsx";
 
 // Recruiter Pages
 import HireResources from "../Requirements/Recruiters/HireTalent.jsx";
-import AddOpportunity from "../Requirements/Recruiters/Addopportunity?.jsx";
+import AddOpportunity from "../Requirements/Recruiters/Addopportunity.jsx";
 import TalentPool from "../Requirements/Recruiters/TalentPool.jsx";
 import { ListedListenings } from "../Requirements/Recruiters/JobListings/ListedListenings.jsx";
 import { MyJobListings } from "../Requirements/Recruiters/MyJobListings.jsx";
